cc5972bccdea01558866f2dd906504f638627fa4
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2018-11-27 17:41:48 +0700

    Initial Commit

---
+++ AwesomeButton/AwesomeButton.h
@@ -0,0 +1,19 @@
+//
+//  AwesomeButton.h
+//  AwesomeButton
+//
+//  Created by Zulwiyoza Putra on 27/11/18.
+//  Copyright © 2018 Zulwiyoza Putra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AwesomeButton.
+FOUNDATION_EXPORT double AwesomeButtonVersionNumber;
+
+//! Project version string for AwesomeButton.
+FOUNDATION_EXPORT const unsigned char AwesomeButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeButton/PublicHeader.h>
+
+
