d1acb398bcffa0a6a45dab214a7f2c01f7915084
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2018-11-27 17:47:53 +0700

    AwesomeButton.swift

--- AwesomeButton/AwesomeButton.swift
+++ AwesomeButton/AwesomeButton.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class AwesomeButton: UIButton {
+    private override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    convenience public init(withTitle title: String?) {
+        self.init(frame: CGRect.zero)
+        self.titleLabel?.text = title ?? "Click Me"
+        self.layer.cornerRadius = 16.0
+        self.clipsToBounds = true
+        self.backgroundColor = .blue
+        self.tintColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
